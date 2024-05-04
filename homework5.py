c8b2cfe160287713276a3c6e76254068f9bdef34
UmarL9107 <168477540+UmarL9107@users.noreply.github.com>
2024-05-04 18:30:17 +0400

    Практическое задание по теме: "Списки и словари"
    my_list = ['apple', 'melon', 'chery', 'orange', 'strawberry']
    print(my_list)
    print(my_list[0])
    print(my_list[4])
    print(my_list[2:5])
    my_list[2] = 'lemon'
    print(my_list)
    
    my_dict = {'Wood': 'Древесина', 'Forest': 'Лес', 'woodpecker': 'Дятел'}
    print(my_dict)
    print(my_dict.get('woodpecker'))
    my_dict.update({'Leon': 'Лев', 'Wolf': 'Волк'})
    print(my_dict)
    my_dict['Wood'] = 'ДРЕВО'
    print(my_dict)

---
+++ homework5.py
@@ -0,0 +1,15 @@
+my_list = ['apple', 'melon', 'chery', 'orange', 'strawberry']
+print(my_list)
+print(my_list[0])
+print(my_list[4])
+print(my_list[2:5])
+my_list[2] = 'lemon'
+print(my_list)
+
+my_dict = {'Wood': 'Древесина', 'Forest': 'Лес', 'woodpecker': 'Дятел'}
+print(my_dict)
+print(my_dict.get('woodpecker'))
+my_dict.update({'Leon': 'Лев', 'Wolf': 'Волк'})
+print(my_dict)
+my_dict['Wood'] = 'ДРЕВО'
+print(my_dict)
